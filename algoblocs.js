0609dad6acae9ce43965a0287a07c6977d76e3e3
Thomas <thciccarelladev@gmail.com>
2025-06-17 10:27:45 +0200

    exo 1 finis

---
+++ algoblocs.js
@@ -0,0 +1,9 @@
+//=======================================================\\EXO 1//=============================================================
+//posittion de la tortue au millieux de la page
+setPos(300,300);
+//avancer a droite de 200px
+forward(200);
+//orienter a droite
+faceRight();
+//avancer de 100px
+forward(100);
