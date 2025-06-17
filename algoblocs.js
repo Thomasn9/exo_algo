49f095a91df4f70a2abeaafcf16f84c90e667b9f
Thomas <thciccarelladev@gmail.com>
2025-06-17 10:57:34 +0200

    exo 2

--- algoblocs.js
+++ algoblocs.js
@@ -1,9 +1,45 @@
 //=======================================================\\EXO 1//=============================================================
 //posittion de la tortue au millieux de la page
-setPos(300,300);
+//setPos(300,300);
+
 //avancer a droite de 200px
-forward(200);
+//forward(200);
+
 //orienter a droite
-faceRight();
+//faceRight();
+
+//avancer de 100px
+//forward(100);
+
+//=======================================================\\EXO 2//=============================================================
+
+//posittion de la tortue au millieux de la page
+setPos(300,300);
+
+// choix de la couleur
+changeColor(color.yellow);
+
+//épaisseur du trait
+setLineWidth(10)
+
+//orienter a gauche
+faceLeft();
+
 //avancer de 100px
 forward(100);
+
+//orienter en bas
+faceDown();
+
+//avancer de 100px
+forward(100);
+
+//orienter a droite
+faceRight();
+
+//avancer de 200px
+forward(200);
+
+//orienter en haut
+faceUp();
+forward(200);
