5abe6823adb3250659baf1bdf550241fb9e17ff6
Thomas <thciccarelladev@gmail.com>
2025-06-25 11:47:03 +0200

    ajout exo 1 a 7 exo tableau

--- exo_tableau/tableaux.js
+++ exo_tableau/tableaux.js
@@ -1,16 +1,44 @@
 // 1) Créer un tableau tab1 de 6 cases, qui contient les nombres 144, 202, 216, 216, 222 et 64.
+let tab1 = [144,202,216,216,222,64];
+console.log("=====================\\voici le tab1 a son initilisation//=====================");
+console.log(tab1);
 
 // 2) Ajouter le nombre 0 à la fin du tableau tab1.
+tab1.push(0);
+console.log("=====================\\ajout de 0 dans le tableau//=====================");
+console.log(tab1);
 
 // 3) Afficher *un par un* tous les éléments du tableau tab1.
 
+console.log("=====================\\Affichage des éléments 1 par 1//=====================");
+for(let i = 0; i<tab1.length; i++){
+    console.log(i);
+}
+
+
 // 4) Retirer le dernier élément du tableau tab1.
+console.log("=====================\\Retirer le dernier élément du tableau//=====================");
+tab1.pop();
+console.log(tab1);
 
 // 5) Créer un tableau tab2 qui contient les nombres 238, 222, 228, 216, 200.
+console.log("=====================\\Création du tab2//=====================");
+let tab2 = [238,222,228,216,200];
+console.log(tab2);
+
 
 // 6) Ajouter le nombre 58 à la fin de tab2.
+console.log("=====================\\ajout de 58 au tab//=====================");
+tab2.push(58);
+console.log(tab2);
+
 
 // 7) Ajouter *un par un* tous les éléments du tableau tab2 à la fin du tableau tab1.
+console.log("=====================\\ajouter les élément de tab2 a tab1//=====================");
+for (let i = 0; i<tab2.length; i++){
+    tab1.push(tab2[i]);
+}
+console.log(tab1);
 
 // 8) Afficher *un par un* tous les éléments du tableau tab1.
 
